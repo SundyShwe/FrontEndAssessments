6e983e27c170f0c99c74cb9113860db866e70c92
SundyShwe <sundyshwe@gmail.com>
2025-05-31 18:07:26 -0500

    add checkbox for accordion

--- Accordion_HTML_CSS_JS/app.js
+++ Accordion_HTML_CSS_JS/app.js
@@ -1,7 +1,20 @@
 const accordionItems = document.querySelectorAll('.accordion-item');
+const openMultipleItems = document.getElementById('openMultipleCheckbox');
 
 accordionItems.forEach(item => {
     item.addEventListener('click', () => {
-        item.classList.toggle('active');
+        //open it
+        const currentItem = item.closest('.accordion-item');
+        currentItem.classList.toggle('active');
+
+        //close others 
+        if (!openMultipleItems.checked) {
+            accordionItems.forEach(other => {
+                const otherItem = other.closest('.accordion-item');
+                if (otherItem !== currentItem) {
+                    otherItem.classList.remove('active');
+                }
+            })
+        }
     })
 });
